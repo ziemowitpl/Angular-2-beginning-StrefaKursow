c01cefeb09b19bc05ba1312cade61652722ea3fd
Ziemowit Płaszaj <roninrallegro@gmail.com>
2018-09-10 17:39:30 +0200

    Create first articles
    app.component.ts - add import and var ARTICLES: Article[] = [ zawartosc ]; by article.ts

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './article'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var AppComponent;
+    var core_1, article_1;
+    var AppComponent, ARTICLES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (article_1_1) {
+                article_1 = article_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -31,8 +34,13 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            // npm start
+            ARTICLES = [
+                new article_1.Article('Semantric UI', 'http://semantic-ui.com', 'Semantic empowers designers and developers by creating a shared vocabulary for UI.', 'http://semantic-ui.com/images/logo.png', 0),
+                new article_1.Article('Angular 2', 'https://angular.io/', 'Headers may be oriented to give the hierarchy of a section in the context of the page.', 'resources/images/ang.png', 0),
+                new article_1.Article('Co to jest Bower?', 'https://github.com/bower/bower', 'A header provides a short summary of content.', 'http://bower.io/img/bower-logo.png', 0)
+            ];
         }
     }
 });
-// npm start
 //# sourceMappingURL=app.component.js.map
